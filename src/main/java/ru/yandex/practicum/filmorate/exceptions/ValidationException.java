da8f9cec512756fd07007188d7e2ffa14481e562
Fastfat <81297831+Fastfat@users.noreply.github.com>
2022-09-25 10:38:12 +0400

    Update ValidationException.java

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ValidationException extends RuntimeException {
     public ValidationException(final String message) {
         super(message);
