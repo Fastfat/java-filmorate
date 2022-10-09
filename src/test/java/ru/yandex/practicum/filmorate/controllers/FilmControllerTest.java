f7e4b740d5bb2f9794f25918e093cfbf0f517872
Fastfat <81297831+Fastfat@users.noreply.github.com>
2022-10-09 20:18:57 +0400

    Update FilmControllerTest.java

--- src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
@@ -5,7 +5,6 @@ import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +15,7 @@ class FilmControllerTest {
     void addFilmWithEmptyList() {
         ValidationException ex = assertThrows(
                 ValidationException.class, () -> filmController.addFilm(new Film(1, null, "Shark",
-                        LocalDateTime.now(), 23))
+                        LocalDate.now(), 23))
         );
     }
 
@@ -24,7 +23,7 @@ class FilmControllerTest {
     void addFilmWithWrongDataRelease() {
         ValidationException ex = assertThrows(
                 ValidationException.class, () -> filmController.addFilm(new Film(1, "Shark2D",
-                        "Shark", LocalDate.of(1890, 11, 28).atStartOfDay(), 23))
+                        "Shark", LocalDate.of(1890, 11, 28), 23))
         );
     }
 
@@ -32,7 +31,7 @@ class FilmControllerTest {
     void addFilmWithWrongDuration() {
         ValidationException ex = assertThrows(
                 ValidationException.class, () -> filmController.addFilm(new Film(1, "Shark2D",
-                        "Shark", LocalDateTime.now(), -23))
+                        "Shark", LocalDate.now(), -23))
         );
     }
 }
