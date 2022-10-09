69abc1448cc9f2e283cc9d2676af68aa1c558943
Fastfat <81297831+Fastfat@users.noreply.github.com>
2022-10-09 20:23:08 +0400

    Update ValidationException.java

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -3,9 +3,10 @@ package ru.yandex.practicum.filmorate.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.METHOD_NOT_ALLOWED)
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class ValidationException extends RuntimeException {
     public ValidationException(final String message) {
         super(message);
     }
 }
+
