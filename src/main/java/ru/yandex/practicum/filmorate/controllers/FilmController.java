6fabea42dd9c4c79b64d63aa2d507862a4219ab4
Fastfat <81297831+Fastfat@users.noreply.github.com>
2022-10-09 20:20:43 +0400

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -6,24 +6,29 @@ import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class FilmController {
 
     private HashMap<Integer, Film> films = new HashMap<>();
-    private int id = 0;
+    private int id = 1;
     private static final String INCORRECT_RELEASE_DATE = "Неправильная дата релиза фильма";
     private static final String INCORRECT_DURATION = "Неправильная продолжительность фильма";
     private static final String EMPTY_NAME = "Имя фильма не может быть пустым";
+    private static final String UNKNOWN_FILM = "Фильма не существует";
+    private static final String LONG_DESCRIPTION = "Длина описания не должна привышать 200 символов";
 
     @GetMapping("/films")
-    public HashMap<Integer, Film> getAllFilms() {
-        return films;
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
     }
 
-    @PostMapping("/film")
+    @PostMapping("/films")
     public Film addFilm(@RequestBody Film film) {
         log.info("Получен запрос к эндпоинту: POST /film, тело запроса: '{}'", film);
         checkCorrect(film);
@@ -34,7 +39,7 @@ public class FilmController {
         return film;
     }
 
-    @PutMapping("/film")
+    @PutMapping("/films")
     public Film setFilm(@RequestBody Film film) {
         log.info("Получен запрос к эндпоинту: PUT /film, тело запроса: '{}'", film);
         checkCorrect(film);
@@ -42,6 +47,7 @@ public class FilmController {
             films.put(film.getId(), film);
         } else {
             log.warn("Фильм: '{}', не был обновлен, информация о фильме: '{}'", film.getName(), film);
+            throw new ValidationException(UNKNOWN_FILM);
         }
         return film;
     }
@@ -49,13 +55,13 @@ public class FilmController {
     private void checkCorrect(Film film) {
         int limit = 200;
         LocalDate dateRelease = LocalDate.of(1895, 11, 28);
-        String description = film.getDescription().length() > limit ?
-                film.getDescription().substring(0, limit) : film.getDescription();
-        film.setDescription(description);
+        if (film.getDescription().length() > limit) {
+            throw new ValidationException(LONG_DESCRIPTION);
+        }
         if (film.getName() == null || film.getName().isEmpty()) {
             throw new ValidationException(EMPTY_NAME);
         }
-        if (film.getReleaseDate().isBefore(dateRelease.atStartOfDay())) {
+        if (film.getReleaseDate().isBefore(ChronoLocalDate.from(dateRelease.atStartOfDay()))) {
             throw new ValidationException(INCORRECT_RELEASE_DATE);
         }
         if (film.getDuration() < 0) {
