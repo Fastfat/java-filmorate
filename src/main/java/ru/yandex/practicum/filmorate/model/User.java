feaee16b34e6c8f02c9a1c3e2af41345f7d0ad16
Fastfat <81297831+Fastfat@users.noreply.github.com>
2022-10-09 20:22:28 +0400

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,21 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
-import java.time.LocalDateTime;
-import lombok.Data;
+import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class User {
+
     private int id;
     private String email;
     private String login;
     private String name;
-    private LocalDateTime birthday;
-
-    public User(int id, String email, String login, String name, LocalDateTime birthday) {
-        this.id = id;
-        this.email = email;
-        this.login = login;
-        this.name = name;
-        this.birthday = birthday;
-    }
+    private LocalDate birthday;
 }
+
